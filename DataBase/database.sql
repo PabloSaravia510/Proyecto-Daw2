57f5cb9e59ead5abfef1989c1ec3a8c2da574fcd
PabloSaravia510 <akirasaravia12618@gmail.com>
2021-05-07 17:24:38 -0500

    constraint script

--- DataBase/database.sql
+++ DataBase/database.sql
@@ -1,10 +1,11 @@
-CREATE database if not exists `db_proyecto_dawii`;
+DROP DATABASE `db_proyecto_dawii`;
+CREATE database `db_proyecto_dawii`;
 use `db_proyecto_dawii`;
 
 
 drop table if exists `tb_carrera`;
 create table `tb_carrera`(
-COD_CAR int (8) not null primary key,
+COD_CAR int (8) not null auto_increment primary key,
 DES_CAR varchar(250) not null,
 EST_REG char(1) not null
 );
@@ -12,7 +13,7 @@ EST_REG char(1) not null
 
 drop table if exists `tb_alumno`;
 create table `tb_alumno`(
-COD_ALU int (8) not null primary key,
+COD_ALU int (8) not null auto_increment primary key,
 NOM_ALU varchar(150) not null,
 APE_ALU varchar(150) not null,
 USU_ALU char(10) not null,
@@ -27,12 +28,11 @@ EST_REG char(1) not null
 
 drop table if exists `tb_profesor`;
 create table `tb_profesor`(
-COD_PRO int (8) not null primary key,
+COD_PRO int (8) not null auto_increment primary key,
 NOM_PRO varchar(150) not null,
 APE_PRO varchar(150) not null,
 USU_PRO char(10) not null,
 PASS_PRO char(16) not null,
-COD_SEC int(8) not null,
 EDAD_PRO int not null,
 CEL_PRO char(9) not null,
 DIR_PRO varchar(250) not null,
@@ -42,14 +42,14 @@ EST_PRO char(1) not null
 
 drop table if exists `tb_horario`;
 create table `tb_horario`(
-COD_HOR int(8) not null primary key,
+COD_HOR int(8) not null auto_increment primary key,
 DES_HOR datetime not null
 );
 
 
 drop table if exists `tb_seccion`;
 create table `tb_seccion`(
-COD_SEC int(8) primary key not null,
+COD_SEC int(8)  not null auto_increment primary key,
 DES_SEC char(4) not null,
 COD_PRO int(8) not null,
 COD_HOR int(8) not null,
@@ -61,7 +61,7 @@ EST_REG char(1) not null
 
 drop table if exists `tb_clase`;
 create table `tb_clase`(
-COD_CLA int(8) not null primary key,
+COD_CLA int(8) not null auto_increment primary key,
 COD_SEC int(8)  not null,
 EST_REG char(1) not null
 );
@@ -69,7 +69,7 @@ EST_REG char(1) not null
 
 drop table if exists `tb_asistencia`;
 create table `tb_asistencia`(
-COD_ASI int (8) not null primary key,
+COD_ASI int (8) not null auto_increment primary key,
 COD_ALU int(8) not null,
 COD_CLA int(8) not null,
 EST_REG char(1) not null
@@ -77,10 +77,9 @@ EST_REG char(1) not null
 
 
 
-
 drop table if exists `tb_administrador`;
 create table `tb_administrador`(
-COD_ADMIN int (8) not null primary key,
+COD_ADMIN int (8) not null auto_increment primary key,
 NOM_ADMIN varchar(150) not null,
 APE_ADMIN varchar(150) not null,
 USU_ADMIN char(10) not null,
@@ -95,13 +94,57 @@ EST_REG char(1) not null
 );
 
 
-
 drop table if exists `tb_geolocalizacion`;
 create table `tb_geolocalizacion`(
-COD_GEO int(8) primary key not null,
+COD_GEO int(8)  not null auto_increment primary key,
 COR_GEO char(4) not null,
-COD_ALU char(1) not null
+COD_ALU int(8) not null
 );
+
+
+
+drop table if exists `tb_QR`;
+create table `tb_QR`(
+COD_QR int(8)  not null auto_increment primary key,
+DES_QR varchar(50) not null,
+FEC_QR datetime not null
+);
+
+
+-------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE tb_alumno
+	ADD	CONSTRAINT FK_ALU_COD_CAR_ID FOREIGN KEY (COD_CAR) REFERENCES tb_carrera (COD_CAR);
+ALTER TABLE tb_seccion
+	ADD	CONSTRAINT FK_SEC_COD_PRO_ID FOREIGN KEY (COD_PRO) REFERENCES tb_profesor (COD_PRO),
+    ADD	CONSTRAINT FK_SEC_COD_HOR_ID FOREIGN KEY (COD_HOR) REFERENCES tb_horario (COD_HOR);
+ALTER TABLE tb_clase
+	ADD	CONSTRAINT FK_CLA_COD_SEC_ID FOREIGN KEY (COD_SEC) REFERENCES tb_seccion (COD_SEC);
+ALTER TABLE tb_asistencia
+	ADD	CONSTRAINT FK_ASI_COD_ALU_ID FOREIGN KEY (COD_ALU) REFERENCES tb_alumno (COD_ALU),
+    ADD	CONSTRAINT FK_ASI_COD_CLA_ID FOREIGN KEY (COD_CLA) REFERENCES tb_clase (COD_CLA);
+ALTER TABLE tb_administrador
+	ADD	CONSTRAINT FK_ADMIN_COD_SEC_ID FOREIGN KEY (COD_SEC) REFERENCES tb_seccion (COD_SEC),
+    ADD	CONSTRAINT FK_ADMIN_COD_PRO_ID FOREIGN KEY (COD_PRO) REFERENCES tb_profesor (COD_PRO),
+    ADD	CONSTRAINT FK_ADMIN_COD_ALU_ID FOREIGN KEY (COD_ALU) REFERENCES tb_alumno (COD_ALU);
+ALTER TABLE tb_geolocalizacion
+	ADD	CONSTRAINT FK_GEO_COD_ALU_ID FOREIGN KEY (COD_ALU) REFERENCES tb_alumno (COD_ALU);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
