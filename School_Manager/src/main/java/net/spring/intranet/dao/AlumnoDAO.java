a66826b9958fd7f0062fafc38fda9acc8018a73e
paulable <carlospgp13@gmail.com>
2021-06-06 00:12:52 -0500

    terminando daos alumno y profesor

--- School_Manager/src/main/java/net/spring/intranet/dao/AlumnoDAO.java
+++ School_Manager/src/main/java/net/spring/intranet/dao/AlumnoDAO.java
@@ -1,7 +1,17 @@
 package net.spring.intranet.dao;
 
+import java.util.List;
+
 import net.spring.intranet.entity.Alumno;
 
 public interface AlumnoDAO {
 	public Alumno iniciarSesion(String user, String pass, int codRol);
+	
+	public void actualizar(Alumno bean);
+	
+	public void eliminar(int cod);
+	
+	public List<Alumno> listarAlumnos();
+	
+	public void buscar(int cod);
 }
--- School_Manager/src/main/java/net/spring/intranet/dao/ProfesorDAO.java
+++ School_Manager/src/main/java/net/spring/intranet/dao/ProfesorDAO.java
@@ -1,7 +1,18 @@
 package net.spring.intranet.dao;
 
+import java.util.List;
+
+import net.spring.intranet.entity.Alumno;
 import net.spring.intranet.entity.Profesor;
 
 public interface ProfesorDAO {
 	public Profesor iniciarSesion(String user, String pass, int codRol);
+	
+	public void actualizar(Profesor bean);
+	
+	public void eliminar(int cod);
+	
+	public List<Alumno> listarProfesores();
+	
+	public void buscar(int cod);
 }
